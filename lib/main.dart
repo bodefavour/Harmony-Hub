5ec3db56e3de9459cd3b7d96f0b420d4078d0a45
bodefavour <bodefavour@gmail.com>
2025-10-27 15:58:36 +0100

    style: Clean up formatting in _MyAppState class for improved readability

--- lib/main.dart
+++ lib/main.dart
@@ -52,16 +52,16 @@ class _MyAppState extends State<MyApp> {
 
     _appStateNotifier = AppStateNotifier.instance;
     _router = createRouter(_appStateNotifier);
-    
+
     // Initialize with a default user immediately to unblock splash screen
     _appStateNotifier.update(HarmonyHubSupabaseUser(null));
-    
+
     // Then listen for actual auth changes
     userStream = harmonyHubSupabaseUserStream()
       ..listen((user) {
         _appStateNotifier.update(user);
       });
-      
+
     // Hide splash screen after brief delay
     Future.delayed(
       const Duration(milliseconds: 1000),
